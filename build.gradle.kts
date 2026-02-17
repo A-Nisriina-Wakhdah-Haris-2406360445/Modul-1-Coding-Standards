21ffdfeb2b1e7512f6ec2ac692d8c6d3c3554267
NisriinaWakhdah <ninaputri3344@gmail.com>
2026-02-17 13:08:25 +0700

    Implement SonarCloud analysis

--- build.gradle.kts
+++ build.gradle.kts
@@ -86,7 +86,7 @@ tasks.jacocoTestReport {
 sonar {
     properties {
         property("sonar.projectKey", "A-Nisriina-Wakhdah-Haris-2406360445_Modul-2-CI-CD-DevOps")
-        property("sonar.organization", "A-Nisriina-Wakhdah-Haris-2406360445")
+        property("sonar.organization", "a-nisriina-wakhdah-haris-2406360445")
         property("sonar.host.url", "https://sonarcloud.io")
         property("sonar.java.source", "21")
     }
